e1eeb90f2a436054493da09df0e7f07277c9ecb8
Simon Kirby <61316399+simonkirbysimonkirby@users.noreply.github.com>
2022-06-15 10:36:46 +0100

    Added method to convert straight skeletons to line segments.

--- 1_building_layouts/geometry_processing.py
+++ 1_building_layouts/geometry_processing.py
@@ -2,7 +2,7 @@ import pickle
 import os
 import skgeom as sg
 
-from building_visualisation import plot_doorways_and_rooms
+from building_visualisation import plot_doorways_and_rooms, plot_building_skeletons
 
 
 def load_pickle(filename):
@@ -46,6 +46,24 @@ def create_clinic_skeletons(polygon_dict):
     return {room_name: _create_single_room_skeletons(polygon) for room_name, polygon in polygon_dict.items()}
 
 
+def create_skeleton_line_dict(skeleton_dict):
+    """Convert skeleton dict to line dict"""
+    room_segment_dict = {}
+    for room_name, skeleton in skeleton_dict.items():
+        room_segments = []
+        for h in skeleton.halfedges:
+            if h.is_bisector:
+                p1 = h.vertex.point
+                p2 = h.opposite.vertex.point
+                segment = sg.Segment2(p1, p2)
+                room_segments.append(segment)
+
+        room_segments = room_segments[1::2]
+        room_segment_dict[room_name] = room_segments
+
+    return room_segment_dict
+
+
 
 if __name__ == "__main__":
     polygon_dict = load_pickle("room_polygons.pickle")
@@ -53,10 +71,12 @@ if __name__ == "__main__":
 
     doorway_location_dict = building_doorway_dict['doorway_location_dict']
     doorway_connection_dict = building_doorway_dict['doorway_info_dict']
-
     plot_doorways_and_rooms(polygon_dict, doorway_location_dict)
 
     skeleton_dict = create_clinic_skeletons(polygon_dict)
+    plot_building_skeletons(polygon_dict, skeleton_dict, doorway_location_dict)
+
+    room_segment_dict = create_skeleton_line_dict(skeleton_dict)
 
 
 
