f92f15cd8abf5720b20f5d53cbcc011bd92b705c
Simon Kirby <61316399+simonkirbysimonkirby@users.noreply.github.com>
2022-06-15 11:01:14 +0100

    Added visualisation for cut and connecting line segments.

--- 1_building_layouts/building_visualisation.py
+++ 1_building_layouts/building_visualisation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from descartes import PolygonPatch
+import numpy as np
 
 
 def plot_doorways_and_rooms(polygon_dict, doorway_dict):
@@ -71,4 +72,48 @@ def plot_building_skeletons(polygon_dict, skeleton_dict, doorway_dict):
 
     ax.axis('equal')
     plt.title("Straight skeletons of each room in building")
+    plt.show()
+
+
+def plot_building_line_segments(updated_room_segment_dict, connecting_segment_dict, polygon_dict, doorway_dict):
+    fig, ax = plt.subplots()
+    fig.set_size_inches(18, 10)
+
+    for name, polygon in polygon_dict.items():
+        patch = PolygonPatch(polygon.buffer(0), fc='white', linewidth=4)
+        ax.add_patch(patch)
+
+    for room_name, coords in doorway_dict.items():
+        ax.plot([i[0] for i in coords],
+                [i[1] for i in coords],
+                linestyle='solid',
+                linewidth=8,
+                markersize=10,
+                c='white')
+
+    for connecting_segment in connecting_segment_dict.values():
+        x1, y1 = connecting_segment[0][0].x(), connecting_segment[0][0].y()
+        x2, y2 = connecting_segment[0][1].x(), connecting_segment[0][1].y()
+
+        ax.plot([x1, x2],
+                [y1, y2],
+                linestyle='solid',
+                linewidth=4,
+                c='r')
+
+    for room, segment_list in updated_room_segment_dict.items():
+        for segment in segment_list:
+            x1, y1 = segment[0].x(), segment[0].y()
+            x2, y2 = segment[1].x(), segment[1].y()
+
+            ax.plot([x1, x2],
+                    [y1, y2],
+                    linestyle='solid',
+                    linewidth=2,
+                    c=np.random.rand(3, ))
+
+    ax.axis('equal')
+    plt.title("Straight skeletons cut into line segments")
+    plt.xlabel('x position, m')
+    plt.ylabel('y position, m')
     plt.show()
