e8d3f556d206be05762e61c27aaf468ecc9b009e
Simon Kirby <61316399+simonkirbysimonkirby@users.noreply.github.com>
2022-06-15 13:47:54 +0100

    Added methods for finding the shortest path between two nodes in the building.

--- 2_building_navigation/src/navigation.py
+++ 2_building_navigation/src/navigation.py
@@ -1,5 +1,15 @@
 import random
 import networkx as nx
+import pickle
+import os
+
+
+def load_pickle(filename):
+    resource_dir = "data/"
+    filepath = os.path.join(resource_dir, filename)
+    with open(filepath, 'rb') as handle:
+        return pickle.load(handle)
+
 
 def select_two_random_nodes(G):
 
@@ -16,6 +26,9 @@ def select_two_random_nodes(G):
     return node_1, node_2
 
 
-def find_shortest_path_3d(G, node_1, node_2):
+def find_shortest_path(G, node_1, node_2):
 
-    return nx.shortest_path(G, source=node_1, target=node_2, weight='weight')
+    shortest_path = nx.shortest_path(G, source=node_1, target=node_2, weight='weight')
+    shortest_path_length = nx.shortest_path_length(G, source=node_1, target=node_2, weight='weight')
+
+    return shortest_path, shortest_path_length
