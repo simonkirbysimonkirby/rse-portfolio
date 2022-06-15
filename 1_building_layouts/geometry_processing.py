cbf570c9ea772dd14024d18f35b2d8af3dff62e8
Simon Kirby <61316399+simonkirbysimonkirby@users.noreply.github.com>
2022-06-15 11:02:10 +0100

    Added methods to cut and connect straight skeletons, creating a group of cut line segments.

--- 1_building_layouts/geometry_processing.py
+++ 1_building_layouts/geometry_processing.py
@@ -1,8 +1,9 @@
 import pickle
 import os
 import skgeom as sg
+import numpy as np
 
-from building_visualisation import plot_doorways_and_rooms, plot_building_skeletons
+from building_visualisation import plot_doorways_and_rooms, plot_building_skeletons, plot_building_line_segments
 
 
 def load_pickle(filename):
@@ -64,6 +65,100 @@ def create_skeleton_line_dict(skeleton_dict):
     return room_segment_dict
 
 
+def _create_intersection_segment(doorway_coords):
+    """Create perpendicular intersection segment/connection path through centre of door, using doorway coordinates"""
+    doorway_p1 = sg.Point2(doorway_coords[0][0], doorway_coords[0][1])
+    doorway_p2 = sg.Point2(doorway_coords[1][0], doorway_coords[1][1])
+
+    doorway_midpoint = 0.5 * (doorway_coords[0][0] + doorway_coords[1][0]), 0.5 * (doorway_coords[0][1] + doorway_coords[1][1])
+    skgeom_doorway_midpoint = sg.Point2(doorway_midpoint[0], doorway_midpoint[1])
+
+    # Create perpendicular ray through midpoint
+
+    intersection_vector_half_length = 20
+    doorway_vector = sg.Vector2(doorway_p1, doorway_p2)
+    perp_doorway_vector_positive = intersection_vector_half_length * doorway_vector.perpendicular(sg.Sign.POSITIVE)
+    perp_doorway_vector_negative = intersection_vector_half_length * doorway_vector.perpendicular(sg.Sign.NEGATIVE)
+
+    positive_endpoint = skgeom_doorway_midpoint + perp_doorway_vector_positive
+    negative_endpoint = skgeom_doorway_midpoint + perp_doorway_vector_negative
+
+    intersection_segment = sg.Segment2(positive_endpoint, negative_endpoint)
+
+    return intersection_segment, skgeom_doorway_midpoint
+
+
+def _find_room_intersection_point(room_segment_list, intersection_segment, skgeom_doorway_midpoint):
+    """Helper function: Given an intersection segment and a room/room segment list, returns a cut room segment list,
+    and the intersection point"""
+
+    # Find the intersections between the intersection segment and the room geometry, storing everything
+
+    untouched_room_segments, segments_with_intersections, segment_lengths, intersection_points = [], [], [], []
+
+    for segment in room_segment_list:
+        intersection_point = sg.intersection(segment, intersection_segment)
+        if intersection_point:
+            possible_intersection_segment = sg.Segment2(skgeom_doorway_midpoint, intersection_point)
+            possible_intersection_length_squared = possible_intersection_segment.squared_length()
+            segments_with_intersections.append(segment)
+            segment_lengths.append(possible_intersection_length_squared)
+            intersection_points.append(intersection_point)
+
+        else:
+            untouched_room_segments.append(segment)
+
+    # Find the intersection segment with the smallest length squared. This is the point to cut at.
+    cut_segments = []
+    index_min = np.argmin(segment_lengths)
+    for idx, segment in enumerate(segments_with_intersections):
+        if idx == index_min:
+            cut_point = intersection_points[idx]
+            sub_segment_1 = sg.Segment2(segment.point(0), cut_point)
+            sub_segment_2 = sg.Segment2(segment.point(1), cut_point)
+
+            cut_segments.append(sub_segment_1)
+            cut_segments.append(sub_segment_2)
+
+        else:
+            untouched_room_segments.append(segment)
+
+    # Finally create the new room segment list
+    updated_room_segment_list = untouched_room_segments + cut_segments
+
+    return updated_room_segment_list, cut_point
+
+
+def find_doorway_intersections(room_segment_dict, doorway_dict, doorway_info_dict):
+    """For each doorway, find midpoint, project ray out and find the intersection with the room segments"""
+    connecting_segment_dict = {}
+    for room_name, doorway_coords in doorway_dict.items():
+        room_segment_list = room_segment_dict[room_name]
+        intersection_segment, skgeom_doorway_midpoint = _create_intersection_segment(doorway_coords)
+
+        # Find intersection with parent room
+        updated_room_segment_list, doorway_connection_1 = _find_room_intersection_point(room_segment_list,
+                                                                                       intersection_segment,
+                                                                                       skgeom_doorway_midpoint)
+
+        # Find intersections with other connecting room
+        other_connecting_room = doorway_info_dict[room_name]
+        connecting_room_segment_list = room_segment_dict[other_connecting_room]
+        updated_connecting_room_segment_list, doorway_connection_2 = _find_room_intersection_point(connecting_room_segment_list,
+                                                                                                  intersection_segment,
+                                                                                                  skgeom_doorway_midpoint)
+
+        # Create the connecting segment and store in a dict (so we can relate to the doorway info if required)
+        connecting_segment = sg.Segment2(doorway_connection_1, doorway_connection_2)
+        connecting_segment_dict[room_name] = [connecting_segment]
+
+        # Update the segment lists
+        room_segment_dict[room_name] = updated_room_segment_list
+        room_segment_dict[other_connecting_room] = updated_connecting_room_segment_list
+
+    return room_segment_dict, connecting_segment_dict
+
+
 
 if __name__ == "__main__":
     polygon_dict = load_pickle("room_polygons.pickle")
@@ -77,6 +172,14 @@ if __name__ == "__main__":
     plot_building_skeletons(polygon_dict, skeleton_dict, doorway_location_dict)
 
     room_segment_dict = create_skeleton_line_dict(skeleton_dict)
+
+    updated_room_segment_dict, connecting_segment_dict = find_doorway_intersections(room_segment_dict,
+                                                                                    doorway_location_dict,
+                                                                                    doorway_connection_dict)
+    plot_building_line_segments(updated_room_segment_dict,
+                               connecting_segment_dict,
+                               polygon_dict,
+                               doorway_location_dict)
 
 
 
