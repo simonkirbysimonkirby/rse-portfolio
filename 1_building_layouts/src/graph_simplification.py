df47e5ebcdb8372918a16392bb91c91eb62aef3c
Simon Kirby <61316399+simonkirbysimonkirby@users.noreply.github.com>
2022-06-16 01:08:17 +0100

    Added docstrings to file.

--- 1_building_layouts/src/graph_simplification.py
+++ 1_building_layouts/src/graph_simplification.py
@@ -6,7 +6,16 @@ from visualisation import plot_clinic_network
 
 
 def _trim_graph(G):
-    """Trim nodes on graph (order < 2)"""
+    """Trims all nodes with order of 1 from the graph, with no checks of distance, and no recursion.
+
+    Args:
+        G (networkx graph object): graph of building
+
+    Returns:
+        L (networkx graph object): copy of G with order 1 nodes removed
+        len(leaves): number of order 1 nodes removed
+    """
+
     L = G.copy()
     leaves = set()
     for node, degree in L.degree():
@@ -18,14 +27,35 @@ def _trim_graph(G):
 
 
 def _euclidean_distance(coord_tuple_1, coord_tuple_2):
-    """Euclidean distance between two points"""
+    """Finds the Euclidean distance between two coordinate tuples (in 2D).
+
+    Args:
+        coord_tuple_1 (tuple): x, y coordinate pair 1
+        coord_tuple_2 (tuple): x, y coordinate pair 2
+
+    Returns:
+        round(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5, 4) (float): Euclidean distance between two points, rounded
+    """
+
     x1, y1 = coord_tuple_1
     x2, y2 = coord_tuple_2
     return round(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5, 4)
 
 
 def _remove_close_order_1_nodes(G, threshold):
-    """Check order 1 nodes, and remove them if they are too close to another node"""
+    """Identifies order 1 nodes that are closer than a threshold distance to their nearest connected neighbour. If
+    they are within the threshold, the node is removed. This function will be called until no nodes are found within
+    the distance threshold.
+
+    Args:
+        G (networkx graph object): graph of building
+        threshold (float): threshold distance for removing order 1 nodes
+
+    Returns:
+        L (networkx graph object): copy of G with order 1 nodes removed
+        number_nodes_removed (int): number of nodes removed in this pass
+    """
+
     L = G.copy()
     short_distance_leaves = set()
     for node, degree in L.degree():
@@ -45,7 +75,17 @@ def _remove_close_order_1_nodes(G, threshold):
 
 
 def _find_single_close_node_pair(G, distance_threshold):
-    """Find a single node pair that is close """
+    """Finds a single close node pair in the graph, within a distance threshold distance. This pair will be contracted
+    in another function.
+
+    Args:
+        G (networkx graph object): graph of building
+        distance_threshold (float): threshold distance for contracting a pair
+
+    Returns:
+        contract_pair (tuple): pair of nodes within a distance threshold that will be contracted
+    """
+
     contract_pair = None
     for node, data in G.nodes(data=True):
         node_coord_tuple = G.nodes[node]['coords']
@@ -60,12 +100,30 @@ def _find_single_close_node_pair(G, distance_threshold):
 
 
 def _contract_network_nodes(G, node_1, node_2):
-    """Function to merge nodes a single time"""
+    """Function to contract a pair of nodes in a graph. Care is taken to ensure that self loops are not formed.
+
+    Args:
+        G (networkx graph object): graph of building
+        node_1 (str): node 1 in contract pair that will be merged
+        node_1 (str): node 2 in contract pair that will be merged
+
+    Returns:
+        nx.contracted_nodes(G, node_1, node_2, self_loops=False) (networkx graph object): graph with one less pair of
+        close nodes
+    """
     return nx.contracted_nodes(G, node_1, node_2, self_loops=False)
 
 
 def _remove_contract_attribute(G):
-    """Function to delete contraction attributes from the graph"""
+    """Function to remove contract attribute (dirt from the contract process).
+
+    Args:
+        G (networkx graph object): graph of building
+
+    Returns:
+        G (networkx graph object): graph of building with contract attribute removed
+    """
+
     attribute_name = 'contraction'
     for node, data in G.nodes(data=True):
         if attribute_name in data.keys():
@@ -75,7 +133,18 @@ def _remove_contract_attribute(G):
 
 
 def _contract_graph(G, threshold):
-    """Identify close nodes in the clinic graph, and then contract them, returning a new graph"""
+    """While True, keep identifying close pairs in the graph within a threshold, and contract the graph. Keep repeating
+    this until no close node pairs are found.
+
+    Args:
+        G (networkx graph object): graph of building
+        threshold (float): threshold distance to contract a pair of nodes
+
+    Returns:
+        G (networkx graph object): graph of building with contract attribute removed
+        count (int): total number of contractions performed
+    """
+
     L = G.copy()
     count = 0
     contract_pair = _find_single_close_node_pair(L, threshold)
@@ -89,7 +158,15 @@ def _contract_graph(G, threshold):
 
 
 def _relabel_graph(G):
-    """Relabel graph based on number of nodes in each room, and update node tag attribute"""
+    """Relabel graph so that node labels start from n1 and increase consecutively.
+
+    Args:
+        G (networkx graph object): graph of building
+
+    Returns:
+        G (networkx graph object): graph of building with updated node labels
+    """
+
     parent_room_dict, mapping_dict, node_tag_dict = {}, {}, {}
     for node, data in G.nodes(data=True):
         parent_room = data['parent_room']
@@ -115,6 +192,22 @@ def _relabel_graph(G):
 
 
 def run_trim_sequence(G, polygon_dict, plot_bool):
+    """Runs the three stage trim sequence:
+        * Stage 1 - removes order 1 geometry from graph
+        * Stage 2 - removes order 1 geometry that is within a threshold distance to its nearest neighbour.
+        * Stage 3 - identifies a close pair of nodes, and contracts the graph at these nodes.
+
+    Stages 2 and 3 are run recursively until 0 nodes can be removed. Plotting is performed, giving visual feedback
+    for each stage.
+
+    Args:
+        G (networkx graph object): graph of building
+        polygon_dict (dict): dictionary containing room polygons
+        plot_bool (bool): if True plots visualisations of each stage
+
+    Returns:
+        contracted_G (networkx graph object): simplified graph of building with trim sequence performed
+    """
 
     # Stage 1
     trimmed_G, nodes_removed = _trim_graph(G)
@@ -141,6 +234,14 @@ def run_trim_sequence(G, polygon_dict, plot_bool):
 
 
 def _set_edge_weights_to_euc_distances(G):
+    """Sets the edge weights as the Euclidean distances between the nodes that bound each edge.
+
+    Args:
+        G (networkx graph object): graph of building
+
+    Returns:
+        G (networkx graph object): graph of building with Euclidean weight information applied
+    """
 
     for node_1, node_2, data in G.edges(data=True):
         node_tuple_1 = G.nodes[node_1]['coords']
@@ -152,6 +253,14 @@ def _set_edge_weights_to_euc_distances(G):
 
 
 def final_graph_processing(G):
+    """Runs the relabelling and edge weight attribute functions.
+
+    Args:
+        G (networkx graph object): graph of building
+
+    Returns:
+        G (networkx graph object): graph of building with Euclidean weight information applied, and relabelled.
+    """
 
     relabelled_G = _relabel_graph(G)
 
@@ -159,6 +268,16 @@ def final_graph_processing(G):
 
 
 def save_graph(G, filename):
+    """Saves the building graph.
+
+    Args:
+        G (networkx graph object): graph of building
+        filename (str): name of output file
+
+    Returns:
+        saved pickle of network (pickle)
+    """
+
     output_dir = "output/"
     filepath = os.path.join(output_dir, filename)
     with open(filepath, 'wb') as handle:
